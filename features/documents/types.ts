e0a9f4ed4a506dc09ff432844c8ed6651d50306d
maciejken <maciejken@gmail.com>
2024-02-17 20:03:10 +0100

    update document types

--- features/documents/types.ts
+++ features/documents/types.ts
@@ -2,19 +2,33 @@ export interface DocumentsState {
   documents: Document[];
 }
 
-type ContentType = 'counter' | 'note';
-
-export interface DocumentContent<T> {
-  type: ContentType;
-  head?: string;
-  body: T;
+type DocumentType = 'counter' | 'note';
+export interface NewDocument {
+  type: DocumentType;
+  title: string;
+  content: string;
+  userId: string;
 }
 
-export interface Document<T = number> {
-  title: string;
-  content?: T;
-  preview?: string;
-  permissions: 'all' | 'modify' | 'view';
-  createdAt?: string;
+export interface Document extends NewDocument {
+  id: string;
+  permissions: Permissions;
+  createdAt: string;
   updatedAt?: string;
 }
+
+enum AccessLevel {
+  None,
+  View,
+  Modify,
+  Manage,
+}
+
+interface Permissions {
+  [id: string]: AccessLevel;
+}
+
+export interface Permission {
+  id: string;
+  value: AccessLevel;
+};
